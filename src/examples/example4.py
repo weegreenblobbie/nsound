1fe64baf88fc80226f32b67cf26025f3af963951
Nick Hilton <Nick Hilton>
2016-07-09 20:53:47 -0700

    changes due to swig interface memory handling, causes seg faults!  need to file bug report

--- src/examples/example4.py
+++ src/examples/example4.py
@@ -4,17 +4,17 @@
 #
 ###############################################################################
 
-from Nsound import *
+import Nsound as ns
 
 sr = 44100.0
 
-sine = Sine(sr)
+sine = ns.Sine(sr)
 
-bass = GuitarBass(sr)
-slide = FluteSlide(sr)
-clarinet = Clarinet(sr)
+bass = ns.GuitarBass(sr)
+slide = ns.FluteSlide(sr)
+clarinet = ns.Clarinet(sr)
 
-out = AudioStream(sr, 1)
+out = ns.AudioStream(sr, 1)
 
 out << bass.play()      << sine.silence(1.0) \
     << slide.play()     << sine.silence(1.0) \
@@ -23,19 +23,22 @@ out << bass.play()      << sine.silence(1.0) \
 out *= 0.5
 
 # Set the default Wavefile sample size and rate.
-Wavefile.setDefaults(sr, 16)
+ns.Wavefile.setDefaults(sr, 16)
 
 out >> "example4.wav"
 
-room = ReverberationRoom(sr, 0.9); # 0.9 = room feed back (0.0 to 1.0)
+room = ns.ReverberationRoom(sr, 0.9); # 0.9 = room feed back (0.0 to 1.0)
 
-out2 = room.filter(0.5 * (bass.play() << sine.silence(1.5)))
+out2 = ns.AudioStream(sr, 1)
+out2 << bass.play() << sine.silence(1.5)
+
+out2 = room.filter(0.5 * out2)
 
 out2 >> "example4_reverb.wav"
 
 # Play to audio device.
 
-pb = AudioPlayback(sr, 2, 16)
+pb = ns.AudioPlayback(sr, 2, 16)
 
 out >> pb
 out2 >> pb
